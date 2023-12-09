faab959a2d40c128a655e47511e7999128e2976b
ShyamnathS <153363369+ShyamnathS@users.noreply.github.com>
2023-12-09 17:10:34 +0530

    Add files via upload

---
+++ assignment/functions.js
@@ -0,0 +1,11 @@
+// Function Declaration
+function greet(name) {
+    console.log("Hello, " + name + "!");
+  }
+  
+  // Function Invocation
+  greet("John"); // Output: Hello, John!
+  greet("Alice"); // Output: Hello, Alice!
+  
+  
+  
